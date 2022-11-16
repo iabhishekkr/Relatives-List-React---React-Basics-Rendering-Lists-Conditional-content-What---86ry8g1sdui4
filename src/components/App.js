fa35d3eb8af9dbf871a8b4d9d2311a7333290627
Abhishek Kumar <abhishek7158@gmail.com>
2022-11-16 15:39:02 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
     render() {
 
-        return(
+        return (
             <div id="main">
-               {/* Do not remove the main div */}
+                {/* Do not remove the main div */}
+                <ol key="relativeList">
+                    <li key="relativeListItem1">Aditya</li>
+                    <li key="relativeListItem2">Saurav</li>
+                    <li key="relativeListItem3">Rahul</li>
+                    <li key="relativeListItem4">Pradeep</li>
+                </ol>
             </div>
         )
     }
